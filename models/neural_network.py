c726bc3b7b70603806b7ab3f30f255839145a3c5
Javier Escribano <escribano101@gmail.com>
2020-10-22 18:57:53 +0200

    Fixed model. Added report.

--- models/neural_network.py
+++ models/neural_network.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class NeuralNetwork(nn.Module):
-    def __init__(self, state_size, action_size, fc1_units=128, fc2_units=64, fc3_units=32):
+    def __init__(self, state_size, action_size, fc1_units=128, fc2_units=64):
         """Initialize parameters and build model.
         Params
         ======
@@ -14,11 +14,11 @@ class NeuralNetwork(nn.Module):
         super(NeuralNetwork, self).__init__()
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, fc3_units)
-        self.fc4 = nn.Linear(fc3_units, action_size)
+        self.fc3 = nn.Linear(fc2_units, action_size)
 
     def forward(self, state):
         """Build a network that maps state -> action values."""
         x = F.elu(self.fc1(state))
         x = F.elu(self.fc2(x))
         return self.fc3(x)
+
